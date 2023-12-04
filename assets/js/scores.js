1269334d653eea3493487f0dcda75b667dd6e7f6
alinakristy <alina.cristi12@gmail.com>
2023-12-04 23:29:41 +0000

    fix leader board'

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,19 +1,24 @@
- var highscore= [
-    { 
-        user : "AA" ,
-        score : 1
-    },
-{
-    user: "BB",
-    score: 2
-} ]
+
  
 var score= document.getElementById('highscores');
+var clearScore=document.getElementById('clear');
+var items=localStorage.getItem('leaderBoards');
+   var highscores=[];
+   if (items!== null){
+    highscores = JSON.parse(items)
+   }
 
-for ( i=0; i<highscore.length; i++){
-    var resultScore= highscore[i]; 
-    var  entry = resultScore.user + " - " + resultScore.score;
+highscores.sort((a,b)=> b.score-a.score)
+
+for ( i=0; i<highscores.length; i++){
+    var resultScore= highscores[i]; 
+    var  entry = resultScore.initials + " - " + resultScore.score;
     var li = document.createElement("li");
     li.textContent= entry ;
     score.appendChild(li);
 };
+
+clearScore.addEventListener('click', function(){
+    localStorage.removeItem('leaderBoards')
+    location.reload();
+})
