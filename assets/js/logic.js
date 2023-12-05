feb5d298e64e071a6515e410f6f802590a16dc41
alinakristy <alina.cristi12@gmail.com>
2023-12-05 11:23:18 +0000

    final Quizz game

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,6 @@
 var timer = document.getElementById("time");
 var startQ = document.getElementById('start');
-var timeLeft = 65;
+
 var startScreen = document.getElementById("start-screen");
 var questionsSection = document.getElementById('questions');
 var answersForQ = document.getElementById('choices');
@@ -10,24 +10,57 @@ var wrongAnswers = document.getElementById('wrongAnswer');
 var endScreen = document.getElementById('end-screen');
 var finalScore = document.getElementById('final-score');
 var stopTimer = false;
+var submit = document.getElementById('submit');
+var enterInitials = document.getElementById('initials');
 
 var quastions = [
     {
-        quastion: 'Choose browser name ',
-        answer: ['Google Crome', 'Lenova', 'iPhone'],
-        rightAnswer: "Google Crome"
+        quastion: 'Where is the correct place to insert a JavaScript?',
+        answer: ['The <head> section', 'The <body>section', ' Both the <head> section and the <body> section are correct'],
+        rightAnswer: 'The <body>section'
     },
     {
-        quastion: '?',
-        answer: ['5', '6', '4'],
-        rightAnswer: "6"
+        quastion: 'How do you create a function in JavaScript?',
+        answer: ['function:myfunction()', 'function = myFunction()', 'function myFunction()'],
+        rightAnswer: 'function myFunction()'
     },
     {
-        quastion: '4 ',
-        answer: ['i', '8', '8'],
-        rightAnswer: "8"
-    }
+        quastion: 'How to write an IF statement in JavaScript?',
+        answer: ['if i = 5 then', 'if i = 5', 'if (i == 5)', 'if i == 5 then'],
+        rightAnswer: 'if (i == 5)'
+    },
+    {
+        quastion: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+        answer: ['if (i <> 5)', 'if (i != 5)', 'if i <> 5', ' if i =! 5 then'],
+        rightAnswer: 'if (i != 5)'
+    }, {
+        quastion: 'How does a WHILE loop start?',
+        answer: ['while (i <= 10)', 'while i = 1 to 10', 'while (i <= 10; i++)'],
+        rightAnswer: 'while (i <= 10)'
+    }, {
+        quastion: 'How can you add a comment in a JavaScript?',
+        answer: ['//This is a comment', 'This is a comment', '<!--This is a comment-->'],
+        rightAnswer: '//This is a comment'
+    }, {
+        quastion: 'What is the correct way to write a JavaScript array?',
+        answer: ['var colors = (1:"red", 2:"green", 3:"blue")', 'var colors = ["red", "green", "blue"]', ' var colors = "red", "green", "blue" '],
+        rightAnswer: 'var colors = ["red", "green", "blue"]'
+    }, {
+        quastion: 'How do you round the number 7.25, to the nearest integer?',
+        answer: ['rnd(7.25)', ' Math.round(7.25)', 'round(7.25)'],
+        rightAnswer: 'Math.round(7.25)'
+    }, {
+        quastion: 'JavaScript is the same as Java.',
+        answer: ['True', 'False'],
+        rightAnswer: 'False'
+    }, {
+        quastion: 'How do you declare a JavaScript variable?',
+        answer: ['v carName', 'variable carName', 'var carName'],
+        rightAnswer: 'var carName'
+    },
 ]
+
+var timeLeft = quastions.length * 2;
 
 var title = document.getElementById('question-title');
 
@@ -38,30 +71,37 @@ function rightAnswerQ() {
 }
 
 function wrongAnswer() {
-    timeLeft -= 5
+    if (timeLeft < 5) {
+        timeLeft = 0
+    } else {
+        timeLeft -= 5
+    }
     incorrectAudio.play();
     wrongAnswers.classList.remove('hide');
     printQ(quastions.pop());
 }
 
 function printQ(question) {
-    if (question === undefined) {
-        showEndScreen()
-    } else {
-        var buttons = answersForQ.querySelectorAll('button')
-        buttons.forEach((button) => answersForQ.removeChild(button))
-        title.textContent = question.quastion
-        for (i = 0; i < question.answer.length; i++) {
-            var answerOption = document.createElement("button")
-            answerOption.textContent = question.answer[i];
-            answersForQ.appendChild(answerOption);
-            if (question.answer[i] == question.rightAnswer) {
-                answerOption.addEventListener('click', rightAnswerQ)
-            } else {
-                answerOption.addEventListener('click', wrongAnswer)
+    if (timeLeft <= 0) {
+        showEndScreen();
+    } else
+        if (question === undefined) {
+            showEndScreen()
+        } else {
+            var buttons = answersForQ.querySelectorAll('button')
+            buttons.forEach((button) => answersForQ.removeChild(button))
+            title.textContent = question.quastion
+            for (i = 0; i < question.answer.length; i++) {
+                var answerOption = document.createElement("button")
+                answerOption.textContent = question.answer[i];
+                answersForQ.appendChild(answerOption);
+                if (question.answer[i] == question.rightAnswer) {
+                    answerOption.addEventListener('click', rightAnswerQ)
+                } else {
+                    answerOption.addEventListener('click', wrongAnswer)
+                }
             }
         }
-    }
 }
 
 function startQuastions() {
@@ -84,11 +124,18 @@ timer.textContent = timeLeft;
 function countdown() {
 
     var timeInterval = setInterval(function () {
-        if (timeLeft === 0 || stopTimer) {
+        if (stopTimer) {
             clearInterval(timeInterval);
-        }else {
-         timeLeft--;
-        timer.textContent = timeLeft;}
+        } else
+            if (timeLeft <= 0) {
+                clearInterval(timeInterval);
+                timeLeft = 0;
+                showEndScreen();
+            } else {
+                timeLeft--;
+            }
+        timer.textContent = timeLeft;
+
     }, 1000);
 }
 
@@ -97,3 +144,17 @@ startQ.addEventListener("click", function () {
     startQuastions();
 });
 
+submit.addEventListener('click', function () {
+    var initials = enterInitials.value;
+    var items = localStorage.getItem('leaderBoards');
+    var players = [];
+    if (items !== null) {
+        players = JSON.parse(items)
+    }
+    players.push({
+        initials: initials,
+        score: timeLeft
+    })
+    localStorage.setItem('leaderBoards', JSON.stringify(players))
+    window.location.href = "highscores.html";
+})
